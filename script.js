0a90c45be99a587ba756dce8efe640de27b2b6fb
Babucarr-Badjie <babucarr.badjie12@gmail.com>
2023-10-18 09:20:49 +0800

    responsiveness

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-const myString = "Speak louder";
-const charArray = [...myString];
-console.log(charArray);
+// Toggle the navbar menu bar
+const menuBar = document.querySelector("#menu-bar");
+const navbar = document.querySelector("#navbar");
+const closeMenuBar = document.querySelector("#close-menu");
+
+if (menuBar) {
+  menuBar.addEventListener("click", () => {
+    navbar.classList.add("active");
+  });
+}
+if (closeMenuBar) {
+  closeMenuBar.addEventListener("click", () => {
+    navbar.classList.remove("active");
+  });
+}

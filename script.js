2d33d1ee7c422c8896c3433e90ede8285e7cff45
Babucarr-Badjie <babucarr.badjie12@gmail.com>
2023-11-12 15:26:23 +0800

    added single product functionality

--- script.js
+++ script.js
@@ -30,3 +30,21 @@ plusButtons.addEventListener("click", (event) => {
   const currentValue = Number(inputField.value) || 1;
   inputField.value = currentValue + 1;
 });
+
+// Toggle slider on singleProduct small images
+let mainImage = document.getElementById("main_image");
+let smallImage = document.getElementsByClassName("small-image");
+
+//   Replacing the main image with small image when clicked on the small image
+smallImage[0].onclick = () => {
+  mainImage.src = smallImage[0].src;
+};
+smallImage[1].onclick = () => {
+  mainImage.src = smallImage[1].src;
+};
+smallImage[2].onclick = () => {
+  mainImage.src = smallImage[2].src;
+};
+smallImage[3].onclick = () => {
+  mainImage.src = smallImage[3].src;
+};
